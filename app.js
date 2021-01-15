49f312d1b5a4e72aa492d438f881ee9dffdd180d
Zach Hornung <Zachhornung@comcast.net>
2021-01-15 15:48:47 -0800

    added multiply function

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function sum(a, b) { //eslint-disable-line
   return result;
 }
 
-// Here is the test for sum(); uncomment it to run it
+// // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -29,11 +29,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var resultMultiply = [];
+  var product = a * b;
+  resultMultiply[0] = product;
+  resultMultiply[1] = "The product of " + a + " and " + b + " is " + product + ".";
+  return resultMultiply;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
