eb6e89047489f5cf20676710a7890cf50c8fae38
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-01-15 15:33:27 -0800

    added solution for multiplication function

--- app.js
+++ app.js
@@ -29,11 +29,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+var resultArray = []
+var multiplication = a * b;
+resultArray[0] = multiplication;
+resultArray[1] = 'The product of ' + a + ' and ' + b + ' is ' + multiplication + '.';
+return resultArray;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
