6fc52ae684a957703e8a58a79b7030250a77a261
Barrett <baxance@protonmail.com>
2021-01-15 16:57:33 -0800

    Problem 1 complete!

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+  let answer = a + b
+  let array = [answer, ('The sum of ' + a + ' and ' + b + ' is ' + answer + '.')]  
+  return array
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+let testSum = sum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
