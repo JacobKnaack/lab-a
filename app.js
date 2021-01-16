3a52bf31558d6c4b9c56b6745bf01d115ef34a1c
Ritzoosk <knecht.ted@gmail.com>
2021-01-16 15:12:19 -0800

    added feature 5 branch

--- app.js
+++ app.js
@@ -116,18 +116,35 @@ testSumArray(testArray);
 
 /////////////////////////////////////
 /* Problem 5
-Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-"The numbers 2,3,4 have a product of 24."
+Write a function called multiplyArray() that takes an array of numbers as its argument and 
+
+
+returns an array whose 
+  first element is the product of those numbers, and the 
+  
+  second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+
+    "The numbers 2,3,4 have a product of 24."
+
+
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements. However, you may continue to use the + operator for string concatenation.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
-}
+var result1 = [];
+  var sum01 = multiply(testArray[0], testArray[1]);
+  var sumAll = multiply(sum01[0], testArray[2]);
+  
+  result1[0] = sumAll[0];
+  result1[1] = 'The numbers ' + testArray + ' have a product of ' +sumAll[0]+ '.';
+  
+  // console.log(prodAB)
+  console.log(result1[1])
+  return result1;}
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
