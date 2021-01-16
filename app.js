bc786159d8f6ba8e34c6181d58d67fc5d2ab5fc6
Jacob Holmer <yacub90@gmail.com>
2021-01-15 17:16:01 -0800

    question 2

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function testSum(a, b) { //eslint-disable-line
+function sum(a, b) { //eslint-disable-line
   var result = [];
   var sum = a + b;
   result [0] = a + b;
-  result [1] = "the sum of" + a + "and " + b + " is " + sum + ".";
+  result [1] = "The sum of " + a + " and " + b + " is " + sum + ".";
     return result;
 
 }
@@ -28,11 +28,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+var result = [];
+var multiply = a * b;
+  result [0] = a * b;
+  result [1] = 'The product of ' + a + ' and ' + b + ' is ' + multiply + '.';
+  return result;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -42,12 +46,18 @@ Write a function called sumAndMultiply() that takes in three numbers as separate
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for te testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+// function sumAndMultiply(a, b, c) { //eslint-disable-line
+// var result = [];
+// // sum(a,b,c);
+// // multiply (a,b,c);
+// result [0] 
 
-}
+
+
+// }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
