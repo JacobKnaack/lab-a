8431b4538b4d9dfb4c9ae2df98ad4e7e26228172
daneng1 <engeldb@gmail.com>
2021-01-16 07:36:27 -0800

    finished mutliply problem

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+
+
 function sum(a, b) { //eslint-disable-line
   var result = [];
   var sum1 = a + b;
@@ -28,11 +30,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var result2 = [];
+  var sum1 = (a * b);
+  
+  result2[0] = sum1;
+  result2[1] = ('The product of ' + a + ' and ' + b + ' is ' + sum1 + '.');
+  return result2;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -46,9 +53,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
 }
-
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
 
