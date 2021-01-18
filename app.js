3435bbe32514381e9036b60169a650d693bd5de2
CoderMichelle <Rosmic1@comcast.net>
2021-01-17 22:28:29 -0800

    adding function multiply

--- app.js
+++ app.js
@@ -27,11 +27,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  var timesArray = [];
+  var c = a * b;
+  timesArray[0] = c;
+  timesArray[1] = "The product of " + a + " and " + b + " is " + c + ".";
+  console.log(timesArray);
+  return timesArray;
 
 }
-
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
