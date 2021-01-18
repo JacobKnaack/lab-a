0fac0c0ffbdcdf6e7a41d1a3d2b31f20a35c75c6
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-01-17 20:17:32 -0800

    third function conquered

--- app.js
+++ app.js
@@ -49,11 +49,24 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var thirdArray = [];
+  // var d = a + b + c;
+  var d = sum(a, sum(b, c)[0])[0];
+  // console.log(d);
+  // var e = a * b * c;
+  var e = multiply(a, b)[0];
+  var f = multiply(1, c)[0];
+  var g = multiply(e, f)[0];
+  // console.log(g);
+  thirdArray[0] = d;
+  thirdArray[1] = g;
+  thirdArray[2] = a + " and " + b + " and " + c + " sum to " + d + ".";
+  thirdArray[3] = "The product of " + a + " and " + b + " and " + c + " is " + g + ".";
+  return thirdArray;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
