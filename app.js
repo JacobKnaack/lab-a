1a2735d1789a6e92f1ffff82257463401257b7f1
Jason Taisey <jdtaisey89@icloud.com>
2021-01-18 18:25:26 -0800

    Problem 1 Fixes

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) {
   var result = [];
   var sum = a + b;
-
-  result[0] = a + b;
-  result[1] = "The sum of " + a + " and " + b + " is " + sum + ".";
+  result[0] = sum;
+  result[1] = "The sum of " + a + ' and ' + b + ' is ' + sum + '.';
   return result;
-}
 
+}
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
+
 // New pull req
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -104,7 +104,7 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
