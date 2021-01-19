03c257d0f9e2a478ae93ad70bf73f674286820d0
Jason Taisey <jdtaisey89@icloud.com>
2021-01-18 16:36:56 -0800

    Fixed Problem 2

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-// function sum(x, y) {
-//   for (var i = 0; i < x.length; i++){
-//     console.log(4[i] + 7)
-//   }
+function sum(a, b) {
+  var result = [];
+  var sum = a + b;
 
-// }
+  result[0] = a + b;
+  result[1] = "The sum of " + a + " and " + b + " is " + sum + ".";
+  return result;
+}
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -29,15 +31,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // }
 
-function sumMult(x,y){
-  var sum = x * y;
-  for (var i = 0; i < x.length; i++)
-  console.log(5[i] * 9)
+function multiply(x, y) {
+  var result = [];
+  var prompt = x * y;
+  
+  result[0] = x * y;
+  result[1] = "The product of " + x + " and " + b + " is " + multiply + ".";
+  return result;
 }
-sumMult(5 * 9)
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
