dee579b4c558cf553a8ffc7de7e1a0e07423d195
Jason Taisey <jdtaisey89@icloud.com>
2021-01-18 16:39:19 -0800

    Problem 2 fix

--- app.js
+++ app.js
@@ -42,6 +42,8 @@ function multiply(x, y) {
 
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5, 9);
+// test
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
